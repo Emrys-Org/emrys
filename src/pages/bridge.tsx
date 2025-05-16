5e479b6e2d8a65ecadb7b646867c07f0c71779de
Afoxcute <jwavolabilove16@gmail.com>
2025-05-16 13:27:16 +0100

    FAQ & WALRUS

--- src/pages/bridge.tsx
+++ src/pages/bridge.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { DisclaimerFooter } from '../components/nav/DisclaimerFooter';
 import { FloatingButtonStrip } from '../components/nav/FloatingButtonStrip';
 import { TransferTokenCard } from '../features/transfer/TransferTokenCard';
 import TestnetBridge from './testnet-bridge';
@@ -16,8 +17,8 @@ const BridgePage: NextPage = () => {
   }, [router.isReady, router.query]);
 
   return (
-    <div className="space-y-3 pt-4">
-      <div className="relative">
+    <div className="space-y-3 pt-4 flex flex-col min-h-[80vh]">
+      <div className="relative flex-grow">
         {isTestnet ? (
           <TestnetBridge />
         ) : (
@@ -26,6 +27,9 @@ const BridgePage: NextPage = () => {
             <FloatingButtonStrip />
           </>
         )}
+      </div>
+      <div className="mt-auto">
+        <DisclaimerFooter />
       </div>
     </div>
   );
